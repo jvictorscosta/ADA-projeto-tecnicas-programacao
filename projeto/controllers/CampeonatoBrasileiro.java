e91aa394d85f2a1ac1ac26dd2ddc22d004ce3bc0
jvistorscosta <jvictorscosta@gmail.com>
2023-10-02 09:39:09 -0300

    Menu

--- projeto/controllers/CampeonatoBrasileiro.java
+++ projeto/controllers/CampeonatoBrasileiro.java
@@ -1,24 +1,24 @@
 package projeto.controllers;
 
 public class CampeonatoBrasileiro {
-    public void execute{
+    public void execute() {
+    showMenu();
 
-        public void showMenu{
-            System.out.println("Olá, bem vindo ao portal do Campeonato Brasileiro");
-            System.out.println("Selecione uma opção abaixo:");
+    }
+    public void showMenu(){
+        System.out.println("Olá, bem vindo ao portal do Campeonato Brasileiro");
+        System.out.println("Selecione uma opção abaixo:");
+        System.out.println();
+        System.out.println("O time que mais venceu jogos no ano 2008");
+        System.out.println("O Estado que teve menos jogos dentro do período 2003 e 2022");
+        System.out.println("O jogador que mais fez gols");
+        System.out.println("O jogador que mais fez gols de pênaltis");
+        System.out.println("O jogador que mais fez gols contras");
+        System.out.println("O jogador que mais recebeu cartões amarelos");
+        System.out.println("O jogador que mais recebeu cartões vermelhos");
+        System.out.println("O placar da partida com mais gols.");
 
-            System.out.println("O time que mais venceu jogos no ano 2008");
-            System.out.println("O Estado que teve menos jogos dentro do período 2003 e 2022");
-            System.out.println("O jogador que mais fez gols");
-            System.out.println("O jogador que mais fez gols de pênaltis");
-            System.out.println("O jogador que mais fez gols contras");
-            System.out.println("O jogador que mais recebeu cartões amarelos");
-            System.out.println("O jogador que mais recebeu cartões vermelhos");
-            System.out.println("O placar da partida com mais gols.");
+    }
 
-        };
-
-
-    };
 
 }
